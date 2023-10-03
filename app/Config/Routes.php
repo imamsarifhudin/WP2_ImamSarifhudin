66035d3edf6b19c5d5ac94b224e0ad934d0b8dd3
imamsarifhudin <imamsarifhudin8723@gmail.com>
2023-10-03 09:41:30 +0700

    tugas akhir slide pertemuan 3

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,7 +5,10 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
+
 $routes->get('contoh1', 'Contoh1::index');
 
 $routes->get('latihan1/penjumlahan/(:num)/(:num)', 'Latihan1::penjumlahan/$1/$2');
 
+$routes->get('matakuliah', 'Matakuliah::index');
+$routes->post('matakuliah/cetak', 'Matakuliah::cetak');
--- app/Controllers/Latihan1.php
+++ app/Controllers/Latihan1.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Controllers;
 
 use App\Controllers\BaseController;
@@ -8,19 +9,19 @@ class Latihan1 extends BaseController
 {
     public function index()
     {
-    // echo "Selamat Datang.. selamat belajar Web Programming";
-    // $this->load->view('view-latihan1');
-    echo "Selamat Datang.. selamat belajar Web Programming";
-    return view('view-latihan1');
+        // echo "Selamat Datang.. selamat belajar Web Programming";
+        // $this->load->view('view-latihan1');
+        echo "Selamat Datang.. selamat belajar Web Programming";
+        return view('view-latihan1');
     }
 
     public function penjumlahan($nilai1, $nilai2)
     {
-    // $this->load->model('Model_latihan1');
-    // $data['nilai1'] = $n1;
-    // $data['nilai2'] = $n2;
-    // $data['hasil'] = $this->Model_latihan1->jumlah($n1, $n2);
-    // $this->load->view('view-latihan1', $data);
+        // $this->load->model('Model_latihan1');
+        // $data['nilai1'] = $n1;
+        // $data['nilai2'] = $n2;
+        // $data['hasil'] = $this->Model_latihan1->jumlah($n1, $n2);
+        // $this->load->view('view-latihan1', $data);
         $model = new Model_Latihan1();
         $data['nilai1'] = $nilai1;
         $data['nilai2'] = $nilai2;
