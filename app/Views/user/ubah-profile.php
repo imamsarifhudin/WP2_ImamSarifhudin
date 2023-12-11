cde1bbc820e86ad5984e877c99fc2544d062ee8f
imamsarifhudin <imamsarifhudin8723@gmail.com>
2023-12-11 22:18:24 +0700

    Tugas Pertemuan 11

--- app/Views/user/ubah-profile.php
+++ app/Views/user/ubah-profile.php
@@ -21,8 +21,7 @@
                 <div class="col-sm-10">
                     <div class="row">
                         <div class="col-sm-3">
-                            <img src="<?=
-                                        base_url('assets/img/profile/') . $user['image']; ?>" class="imgthumbnail" alt="">
+                            <img src="<?=base_url('assets/img/profile/') . $user['image']; ?>" class="imgthumbnail" alt="">
                         </div>
                         <div class="col-sm-9">
                             <div class="custom-file">
